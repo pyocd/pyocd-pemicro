5e28f7dab5a07726e367b1e64c08e3d94ab86c7d
Chris Reed <flit@me.com>
2021-11-27 15:30:45 -0600

    Convert to PEP517 packaging.

--- setup.py
+++ setup.py
@@ -30,56 +30,6 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open('README.md', encoding='utf-8') as readme_file:
-    README = readme_file.read()
-
-setup(
-    name='pyocd-pemicro',
-    use_scm_version={
-        'local_scheme': 'dirty-tag',
-        'write_to': 'pyocd_pemicro/_version.py'
-    },
-    description='PyOCD debug probe plugin for PEMicro debug probes',
-    long_description_content_type="text/markdown",
-    long_description=README,
-    platforms="Windows, Linux, macOS",
-    license='BSD-3-Clause',
-    packages=find_packages(),
-    author='Petr Gargulak, Chris Reed',
-    author_email='petr.gargulak@nxp.com, chris.reed@arm.com',
-    keywords=['PEMicro', 'PyPEMicro', 'PyOCD', 'PyOCD Plugin'],
-    url='https://github.com/pyocd/pyocd-pemicro',
-    download_url='https://pypi.org/project/pyocd_pemicro/',
-    classifiers=[
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: MacOS :: MacOS X',
-        'License :: OSI Approved :: BSD License',
-        'Topic :: Scientific/Engineering',
-        "Topic :: Software Development :: Debuggers",
-        'Topic :: Software Development :: Embedded Systems',
-        'Topic :: System :: Hardware',
-        'Topic :: Utilities'
-        ],
-    python_requires=">=3.6",
-    setup_requires=[
-        'setuptools>=40.0',
-        'setuptools_scm!=1.5.3,!=1.5.4',
-        'setuptools_scm_git_archive',
-        ],
-    install_requires = [
-        'pypemicro==0.1.7',
-        ],
-    entry_points={
-        'pyocd.probe': [
-            'pemicro = pyocd_pemicro:PEMicroProbePlugin',
-        ],
-    },
-    zip_safe=True,
-)
+setup()
